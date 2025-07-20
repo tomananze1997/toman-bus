0b15d96de319faa2381af068c3be5c6adaf0105e
Anže Toman <78642908+tomananze1997@users.noreply.github.com>
2025-07-20 10:25:06 +0200

    feat/mynode.js changes
    -changed how environment file is changed thus making sure that interface
    is not overwritten

--- mynode.js
+++ mynode.js
@@ -4,10 +4,12 @@ const successColor = '\x1b[32m%s\x1b[0m';
 const checkSign = '\u{2705}';
 const dotenv = require('dotenv').config({ path: '.env' });
 
-const envFile = `export const environment = {
-    EMAIL_SERVICE_ID: '${process.env.EMAIL_SERVICE_ID}',
-    EMAIL_TEMPLATE_ID: '${process.env.EMAIL_TEMPLATE_ID}',
-    EMAIL_PUBLIC_KEY: '${process.env.EMAIL_PUBLIC_KEY}',
+const envFile = `import { IEnvironment } from '@interfaces';
+
+export const environment: IEnvironment = {
+  EMAIL_SERVICE_ID: '${process.env.EMAIL_SERVICE_ID}',
+  EMAIL_TEMPLATE_ID: '${process.env.EMAIL_TEMPLATE_ID}',
+  EMAIL_PUBLIC_KEY: '${process.env.EMAIL_PUBLIC_KEY}'
 };
 `;
 const targetPath = path.join(__dirname, './src/app/environments/environments.ts');
